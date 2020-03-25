7762fe4980abcbb85be12a2dc9e7285896e03f4e
Piotr Wachulec <p.wachulec@gmail.com>
2020-03-25 19:25:54 +0100

    Add some logic in Main method

--- ObserverPattern/BaseImplementation/Program.cs
+++ ObserverPattern/BaseImplementation/Program.cs
@@ -7,6 +7,24 @@ namespace BaseImplementation
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var concreteSubject = new ConcreteSubject();
+            var observer1 = new ConcreteObserver("Observer 1");
+            var observer2 = new ConcreteObserver("Observer 2");
+
+            concreteSubject.ConcreteSubjectState = 2;
+
+            concreteSubject.Attach(observer1);
+            observer1.Update(concreteSubject);
+
+            concreteSubject.Attach(observer2);
+            observer2.Update(concreteSubject);
+
+            concreteSubject.ConcreteSubjectState = 3;
+
+            concreteSubject.Detach(observer1);
+
+            concreteSubject.ConcreteSubjectState = 4;
         }
     }
 }
